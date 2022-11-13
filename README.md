9e0004754167817e403990faf57807254124774f
aamrixx <amrix@hotmail.co.uk>
2022-11-13 08:11:11 -0500

    Basic lexer

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # elk
 A really simple programming language written in JavaScript as a little excercise
+
+# Syntax
+Elk has a very Lisp like syntax (kind of) which means it uses Polish Notation<br>
+for example when adding 2 and 2 in most languages it would be. ``2+2`` but in Elk<br>
+it is ``+ 2 2`` this is what Polish notation is. I have tried to base almost all<br>
+of the syntax off this Polish notation with exceptions in a few places<br>
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const TokenKind = {
     Iden: 'Iden',
+    Num: 'Num',
     Add: '+',
     Sub: '-',
     Mul: '*',
@@ -18,19 +19,78 @@ class Lexer {
         this.pos = 0;
     }
 
-    advance() {
-
+    #advance() {
+        this.pos += 1;
     }
     
-    get_char() {
+    #get_char() {
         return this.line[this.pos];
     }
 
-    lex() {
+    #lex_symbol(c) {
+        switch (c) {
+            case '+':
+                return new Token(TokenKind.Add, "+");
+            case '-':
+                return new Token(TokenKind.Sub, "-");
+            case '*':
+                return new Token(TokenKind.Mul, "*");
+            case '/':
+                return new Token(TokenKind.Div, "/");
+            default:
+                return new Token(null, "");
+        }
+    }
 
+    #lex_keyword(s) {
+        if (typeof(s) != "string") {
+            throw "lex_keyword expected a string but got ", typeof(s);
+        }
+
+        switch (s) {
+            case "":
+                return new Token(null, "");
+            default:
+                if (!isNaN(s)) {
+                    return new Token(TokenKind.Num, s);
+                    
+                }
+                return new Token(TokenKind.Iden, s);
+        }
+    }
+
+    lex() {
+        while (this.pos < this.line.length) {
+            let token = this.#lex_symbol(this.#get_char());
+
+            if (token.kind != null) {
+                this.tokens.push(token);
+            } else {
+                let buffer = "";
+
+                while (this.pos < this.line.length && this.#get_char() != ' ') {
+                    buffer += this.#get_char();
+                    this.#advance();
+                }
+
+                let keyword = this.#lex_keyword(buffer);
+                
+                if (keyword.kind != null) {
+                    console.log("wtf");
+                    this.tokens.push(keyword);
+                }
+            }
+
+            this.#advance();
+        }
     }
 }
 
 function main() {
+    const lexer = new Lexer("+ 2 2");
+    lexer.lex();
 
+    console.log(lexer.tokens);
 }
+
+main();
